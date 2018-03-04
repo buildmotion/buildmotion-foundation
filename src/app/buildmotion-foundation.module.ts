9bc2f81a9a46f4d9577ccb924eb3da3c99f2bcb5
joe.gorilla <matt@hybridmobileapp.net>
2018-03-04 08:30:24 -0700

    removing BuildMotionLoggingModule import;

--- src/app/buildmotion-foundation.module.ts
+++ src/app/buildmotion-foundation.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BuildMotionLoggingModule } from '@buildmotion/logging';
-
 @NgModule({
   imports: [
-    CommonModule,
-    BuildMotionLoggingModule
+    CommonModule
   ],
   declarations: []
 })
